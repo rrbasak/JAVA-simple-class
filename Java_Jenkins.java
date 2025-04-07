45bad70aea442793ff761b901474082bb487c7a7
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-04-07 21:38:12 +0530

    Initial commit

---
+++ Java_Jenkins.java
@@ -0,0 +1,7 @@
+public class Java_Jenkins {
+    public static void main(String[] args) {
+        System.err.println("Hello, Jenkins!");
+        System.out.println("how are you?");
+        System.out.println("I am fine, thank you!");
+    }
+}
