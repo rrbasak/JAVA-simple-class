088f45ed33e5f484a5625381b3f7556980946fab
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-04-07 21:48:42 +0530

    2nd commit

--- Java_Jenkins.java
+++ Java_Jenkins.java
@@ -1,8 +1,12 @@
+import java.util.Date;
+
 public class Java_Jenkins {
     public static void main(String[] args) {
         System.err.println("Hello, Jenkins!");
         System.out.println("how are you?");
         System.out.println("I am fine, thank you!");
-        System.out.println(" I am learning Jenkins!");
+        System.out.println("I am learning Jenkins!");
+        System.out.println(new Date());
+        System.out.println("I am learning Jenkins! 2nd time");
     }
 }
